bdde399390a8c42a4174ce2d6905a216c9b1da13
Amitoshverm <amitosh.verma9@gmail.com>
2025-05-02 23:01:26 +0530

    srp done
    Signed-off-by: Amitoshverm <amitosh.verma9@gmail.com>

--- src/SingleResponsibility/Report.java
+++ src/SingleResponsibility/Report.java
@@ -3,8 +3,8 @@ package SingleResponsibility;
 public class Report {
 
     private String reportName;
-    public Report(String reportName) {
-        this.reportName = reportName;
+    public Report(String name) {
+        this.reportName = name;
     }
     public String getReportName() {
         return reportName;
@@ -12,5 +12,17 @@ public class Report {
 
     public void setReportName(String reportName) {
         this.reportName = reportName;
+    }
+
+    // These are the violation of the SRP
+    public void generateReport(Report report) {
+        System.out.println("generating report for: " + report.getReportName());
+    }
+
+    public void saveToFile(String fileName) {
+        System.out.println("saving report to " + fileName);
+    }
+    public void printReport () {
+        System.out.println(reportName);
     }
 }
