34293f17a007bcf69f88913ec9448015ca904478
Amitoshverm <amitosh.verma9@gmail.com>
2025-08-06 22:07:13 +0530

    minima element

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(search(arr));
+    }
+    public static int search(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while (l < r) {
+            int mid = l + (r-l)/2;
+            if (arr[mid] > arr[r]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid;
+            }
+        }
+        return arr[l];
     }
 }
