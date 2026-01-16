b6c82ceec5bff8adbc2d1d24f435b79ca50a308b
Amitoshverm <amitosh.verma9@gmail.com>
2026-01-16 18:20:29 +0530

    comment out

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println(search(arr));
+//        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+//        System.out.println(search(arr));
+
+
     }
     public static int search(int[] arr) {
         int l = 0, r = arr.length - 1;
