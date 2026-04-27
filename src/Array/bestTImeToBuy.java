a150fffd4e89ef2816983b2ed0e3b19365f99000
Amitoshverm <amitosh.verma9@gmail.com>
2026-04-27 17:01:51 +0530

    first done

---
+++ src/Array/bestTImeToBuy.java
@@ -0,0 +1,14 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class bestTImeToBuy {
+    public static void main(String[] args) {
+        System.out.println(add(1,2,5));
+    }
+    static int add (int a, int b, int c) {
+        return a + b + c;
+    }
+}
